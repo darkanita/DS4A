4af1b31b488b6e5373996244cca1ccb65c04c489
darkanita@gmail.com <amlopezmo@unal.edu.co>
2019-10-18 09:19:15 -0500

    commit

--- data.py
+++ data.py
@@ -1,1 +1,41 @@
 #Prueba
+import pandas as pd
+import geopandas
+from shapely.geometry import Point, Polygon
+import tqdm
+import boto3
+from io import StringIO 
+
+def upload_data(data,bucket,key,aws_access_key_id,aws_secret_access_key):
+    '''
+        Upload data to storage S3, aws_acces_key_id and aws_secret_access_key are provided by AWS.
+        This function is only if you require save the information in SW. 
+    '''
+
+    client = boto3.client('s3',aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
+    csv_buffer = StringIO()
+    data.to_csv(csv_buffer, index=False)
+    csv_buffer.seek(0)
+    obj = client.put_object(Bucket=bucket, Key=key, Body=csv_buffer.getvalue(), ACL='public-read')
+
+    return obj
+
+def make_point(latitude, longitude) :
+    '''
+        Return a point from latitude and longitude. 
+    '''
+    return Point( (longitude,latitude) )
+
+def get_ntacode(columnlat,columnlong, datanta, dataori):
+    '''
+        Return dataframe original with the information of NTA Code. 
+    '''
+    for idx in tqdm(range(len(dataori))): 
+        try:
+            dataori['coord'][idx]= make_point(dataori[columnlat][idx], dataori[columnlong][idx])
+            for idy in range(len(datanta)):
+                if dataori['coord'][idx].within(datanta['geometry'][idy]):
+                    dataori['ntacode'][idx] = datanta['ntacode'][idy]
+        except:                   
+            print("Error")
+    return dataori
---
+++ experiment.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from data import *
+
+pd.options.mode.chained_assignment = None 
+
+def main(aws_access_key_id,aws_secret_access_key):
+    bucket = 'DS4A'
+    # NTA
+    NTA = geopandas.read_file('https://ds4adata.s3-sa-east-1.amazonaws.com/Datathon/NTA+map.geojson')
+    print("NTA Read: "+str(NTA.shape))
+    #UBER 2014
+    uber_trips_2014 = pd.read_csv('https://ds4adata.s3-sa-east-1.amazonaws.com/Datathon/uber_trips_2014.csv')
+    print("UBER 2014 Read: "+str(uber_trips_2014.shape))
+    key = 'uber_trips_2014.csv'
+    uber_trips_2014_new = get_ntacode('pickup_latitude','pickup_longitude', NTA, uber_trips_2014)
+    uploadObj = upload_data(uber_trips_2014_new,bucket,key,aws_access_key_id,aws_secret_access_key)
+    print('Uber 2014 Updated: '+ uploadObj)
+    print("UBER 2014 Updated: "+str(uber_trips_2014_new.shape))
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Prepare dataset")
+
+    parser.add_argument('--aws_access_key_id', '-aws_id',
+        help="aws_access_key_id"
+    )
+
+    parser.add_argument('--aws_secret_access_key', '-aws_key',
+        help="aws_secret_access_key"
+    )
+    
+    args = parser.parse_args()
+    main(args.aws_access_key_id, args.aws_secret_access_key)
