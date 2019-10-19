0a6890ca92ac8a1fbcd6d5414a5400f74a2c66c8
darkanita@gmail.com <amlopezmo@unal.edu.co>
2019-10-19 06:34:17 -0500

    commit

--- experiment.py
+++ experiment.py
@@ -21,16 +21,29 @@ def main(aws_access_key_id,aws_secret_access_key):
     #print('Uber 2014 Updated: '+ uploadObj)
     #print("UBER 2014 Updated: "+str(uber_trips_2014_new.shape))
     #GREEN
-    green_trips = pd.read_csv('https://ds4adata.s3-sa-east-1.amazonaws.com/Datathon/green_trips.csv')
-    print(green_trips.head(1))
-    print("GREEN Read: "+str(green_trips.shape))
-    key = folder+'green_trips.csv'
-    green_trips['ntacode']=None
-    green_trips_new = get_ntacode('pickup_latitude','pickup_longitude', NTA, green_trips)
-    uploadObj = upload_data(green_trips_new,bucket,key,aws_access_key_id,aws_secret_access_key)
-    print('Green Updated: '+ uploadObj)
-    print("Green Updated: "+str(green_trips_new.shape))
-    print(green_trips_new.isnull().sum())
+    #green_trips = pd.read_csv('https://ds4adata.s3-sa-east-1.amazonaws.com/Datathon/green_trips.csv')
+    #print(green_trips.head(1))
+    #print("GREEN Read: "+str(green_trips.shape))
+    #key = folder+'green_trips.csv'
+    #green_trips['ntacode']=None
+    #green_trips_new = get_ntacode('pickup_latitude','pickup_longitude', NTA, green_trips)
+    #uploadObj = upload_data(green_trips_new,bucket,key,aws_access_key_id,aws_secret_access_key)
+    #print('Green Updated: '+ uploadObj)
+    #print("Green Updated: "+str(green_trips_new.shape))
+    #print(green_trips_new.isnull().sum())
+    #MTA
+    mta_trips = pd.read_csv('https://ds4adata.s3-sa-east-1.amazonaws.com/Datathon/mta_trips.csv',low_memory=False)
+    print(mta_trips.head(1))
+    print("MTA Read: "+str(mta_trips.shape))
+    key = folder+'mta_trips.csv'
+    mta_trips['ntacode']=None
+    mta_trips_new = get_ntacode('latitude','longitude', NTA, mta_trips)
+    uploadObj = upload_data(mta_trips_new,bucket,key,aws_access_key_id,aws_secret_access_key)
+    print('MTA Updated: '+ uploadObj)
+    print("MTA Updated: "+str(green_trips_new.shape))
+    print(mta_trips_new.isnull().sum())
+
+   
 
 if __name__ == '__main__':
     import argparse
